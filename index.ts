c5e316a56b815183dd8f57192f26bb3dcae47021
Nalithdev <Ethan.delcroix@outlook.fr>
2025-07-23 10:11:53 +0200

    Pass to TS

---
+++ index.ts
@@ -0,0 +1,22 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+
+
+app.use(
+    cors({
+        origin: `${process.env.API_BASE_URL}${process.env.PORT}`,
+        credentials: true,
+    })
+);
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+    console.log(`REST API server ready at: http://localhost:${PORT}`)
+    );

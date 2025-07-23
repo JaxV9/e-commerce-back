008ab3228e11b25610970c4f70c1a38c810027df
Nalithdev <Ethan.delcroix@outlook.fr>
2025-07-23 14:05:32 +0200

    Create API routes

--- index.ts
+++ index.ts
@@ -17,6 +17,41 @@ app.get("/", (req, res) => {
   res.send("Welcome to the REST API!");
 });
 
+app.get(/user\/:id/, (req, res) => {
+    res.send("Users endpoint is under construction.");
+});
+
+app.get(/products/, (req, res) => {
+    res.send("Products endpoint is under construction.");
+});
+
+app.get(/products\/:id/, (req, res) => {
+    const { id } = req.params;
+    res.send(`Product with ID ${id} is under construction.`);
+});
+
+app.post("/product/", (req, res) => {
+    const product = req.body;
+    res.status(201).send(`Product created: ${JSON.stringify(product)}`);
+});
+
+
+app.get(/comments/, (req, res) => {
+    res.send("Products endpoint is under construction.");
+});
+
+app.get(/comments\/:id/, (req, res) => {
+    const { id } = req.params;
+    res.send(`Product with ID ${id} is under construction.`);
+});
+
+app.post("/comment/", (req, res) => {
+    const product = req.body;
+    res.status(201).send(`Comments created: ${JSON.stringify(product)}`);
+});
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`REST API server ready at: http://localhost:${PORT}`)
